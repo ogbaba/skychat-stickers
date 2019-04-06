a826a024f2964ebfcd3714a5a7764479f9df4b16
Baptiste Pouget <baba@firemail.cc>
2019-04-06 22:12:23 +0200

    url

--- stickers.user.js
+++ stickers.user.js
@@ -4,8 +4,8 @@
 // @version      0.2
 // @description  Script permettant d'ajouter soi-même de nouveaux stickers à vos messages redsky.fr.
 // @author       You
-// @updateURL 	 https://raw.githubusercontent.com/ogbaba/skychat-stickers/master/main.js 
-// @downloadURL  https://raw.githubusercontent.com/ogbaba/skychat-stickers/master/main.js
+// @updateURL 	 https://raw.githubusercontent.com/ogbaba/skychat-stickers/master/stickers.user.js 
+// @downloadURL  https://raw.githubusercontent.com/ogbaba/skychat-stickers/master/stickers.user.js
 // @match        https://redsky.fr/
 // @grant        none
 // ==/UserScript==
